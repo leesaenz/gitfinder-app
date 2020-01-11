13e53189a4d8d7aca54fd45600f574460e1bf234
Lee Saenz <leesaenz@gmail.com>
2020-01-11 01:24:20 -0700

    Adds react roouter dom and sets up some routes.

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 //Decouping passed props for use in JSX
@@ -8,7 +9,14 @@ const Navbar = ({ title, icon, search }) => {
       <h1>
         <i className={icon} /> {title}
       </h1>
-      <div>{search}</div>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
