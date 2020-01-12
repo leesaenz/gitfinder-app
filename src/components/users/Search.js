0adf79a80dbb54d07a6b0f28aababd8e3eb5318d
Lee Saenz <leesaenz@gmail.com>
2020-01-11 21:19:06 -0700

    Refactors Users.js to use useEffect hook.

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-//Using a class component here since I want to keep track of state, but will refactor this using React Hooks.
 const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
-  //Initializing state with useState hook
   const [text, setText] = useState('');
 
-  //On change of the search input, the state of text is set to the value of user feedback
   const onChange = e => setText(e.target.value);
 
-  //On form submit, the text is passed to the searchUsers prop, where it triggers the parent as an event.  The text is then cleared.
   const onSubmit = e => {
     e.preventDefault();
     if (text === '') {
